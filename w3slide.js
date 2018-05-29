40a5d8236e0f7ab417703cfa7d655278f2542661
tarivianto <39723743+tarivianto@users.noreply.github.com>
2018-05-30 02:20:15 +0700

    Update w3slide.js

--- w3slide.js
+++ w3slide.js
@@ -1,4 +1,3 @@
-
 var myIndex = 0;
 carousel();
 
@@ -11,5 +10,5 @@ function carousel() {
     myIndex++;
     if (myIndex > x.length) {myIndex = 1}    
     x[myIndex-1].style.display = "block";  
-    setTimeout(carousel, 9000);    
+    setTimeout(carousel, 9000); // Change image every 2 seconds
 }
