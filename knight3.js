ff41da357d6683956993066716f2b1ecc949376a
tarivianto <39723743+tarivianto@users.noreply.github.com>
2018-06-10 17:01:36 +0700

    Create knight3.js

---
+++ knight3.js
@@ -0,0 +1,1 @@
+function disableSelection(e){if(typeof e.onselectstart!="undefined")e.onselectstart=function(){return false};else if(typeof e.style.MozUserSelect!="undefined")e.style.MozUserSelect="none";else e.onmousedown=function(){return false};e.style.cursor="default"}window.onload=function(){disableSelection(document.body)}
