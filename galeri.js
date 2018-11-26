db5592a085a4b0a001461ba9a8aaa5505b3b9296
tarivianto <39723743+tarivianto@users.noreply.github.com>
2018-11-26 10:52:16 +0700

    Create galeri.js

---
+++ galeri.js
@@ -0,0 +1,27 @@
+$(function(){
+   setInterval(function(){
+      $(".slideshow1 ul").animate({marginLeft:-250},800,function(){
+         $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
+      })
+   }, 3500);
+});
+
+
+// controls the animation with mouse over
+$(function(){
+  var timeSlide;
+  function goSlide() {
+      $(".slideshow2 ul").animate({marginLeft:-250},800,function(){
+        $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
+      })
+  }
+  timeSlide = setInterval(goSlide, 3500);
+  
+  $('.slideshow2').on('mouseenter', function(){
+    // stop animation
+    clearInterval(timeSlide);
+  }).on('mouseleave', function(){
+    // play animation
+    timeSlide = setInterval(goSlide, 3500);
+  });
+});
